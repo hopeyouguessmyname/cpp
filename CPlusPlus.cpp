757b295df8da71cfafc92cb051faaae5fdb2848c
Dominik Domagała <Dominik.Domagala@bzwbk.pl>
2017-10-13 22:21:32 +0200

    0002 - Drukowanie funkcji.

--- CPlusPlus.cpp
+++ CPlusPlus.cpp
@@ -1,6 +1,23 @@
+#include <stdio.h>
+#include <string>
+
+
+
+typedef void( * funkcjaT )(); // wskaŸnik na funkcjê
+void print(std::string function_name, funkcjaT pFunkcja);
+
 
 int main()
 {
 
   return 0;
+}
+
+
+void print(std::string function_name, funkcjaT pFunkcja)
+{
+  printf("\n%s\n", function_name.c_str());
+  printf("---------------------------------\n");
+  pFunkcja();
+  printf("---------------------------------\n");
 }
